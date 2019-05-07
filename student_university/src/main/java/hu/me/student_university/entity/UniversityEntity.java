6c0a5f5be90b07a5f3d00da0295d52b6583dc0ac
Toth Peter <toth.peter618@gmail.com>
2019-05-07 17:27:53 +0200

    Student and university entities has been added.

---
+++ student_university/src/main/java/hu/me/student_university/entity/UniversityEntity.java
@@ -0,0 +1,58 @@
+package hu.me.student_university.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class UniversityEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String universityName;
+    private String faculty;
+    private String specialization;
+
+    public  UniversityEntity() {
+
+    }
+
+    public UniversityEntity(Long id, String universityName, String faculty, String specialization) {
+        this.id = id;
+        this.universityName = universityName;
+        this.faculty = faculty;
+        this.specialization = specialization;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+}
---
+++ student_university/src/test/java/hu/me/student_university/StudentUniversityApplicationTests.java
@@ -0,0 +1,16 @@
+package hu.me.student_university;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentUniversityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
